3d1dc9daa0ea538849ff2d589962734bf2bb0d89
pgm-benobira <benoit.biragumaihimbazwe@student.arteveldehs.be>
2024-01-04 21:45:56 +0100

    ✨ Added active compact header calendar view

--- static/js/events.js
+++ static/js/events.js
@@ -1,7 +1,7 @@
 (() => {
 const $events = document.getElementById('events');
 
-// ---------------- ACTIVE VIEW ----------------------------------------------------------------------------------------------------------------------------
+// ---------------- ACTIVE VIEW (list/raster) -------------------------------------------------------------------------------------------------------------
 const $list = document.querySelector('.list');
 const $raster = document.querySelector('.raster');
 
@@ -10,16 +10,16 @@ function changeEventsView() {
         $raster.classList.remove('event-view--active')
         $events.classList.add('events--list')
         $list.classList.add('event-view--active')
-    })
+    });
     $raster.addEventListener('click', () => {
         $list.classList.remove('event-view--active')
         $events.classList.remove('events--list')
         $raster.classList.add('event-view--active')
-    })
-}
+    });
+};
 
 // ---------------- API URL ------------------------------------------------------------------------------------------------------------------------------
-const API_URL = 'https://www.pgm.gent/data/gentsefeesten/events_500.json';
+const API_URL = 'https://www.pgm.gent/data/gentsefeesten/events.json';
 
 // ---------------- FETCH THE DATA -----------------------------------------------------------------------------------------------------------------------
 async function fetchData(url, callback) {
@@ -46,6 +46,25 @@ function filteredEventsByDay(events, selectedDay) {
 
 // ---------------- EVENTS --------------------------------------------------------------------------------------------------------------------------------
 function generateHTMLForEvents(items, category) {
+    // All the items for one category
+    const categoryItems = items.filter((item) => item.category === category)
+
+    // HTML for teasers in one category
+    const teasersHTML = categoryItems.map((item) => `
+    <div class="teaser__wrapper">
+        <span class="teaser__date">${item.day_of_week} ${item.day} juli</span>
+        <img class="teaser__img" src="${item.image ? item.image.thumb : ''}" alt="">
+        <div class="teaser">
+            <h3>${item.title}</h3>
+            <div>
+                <p class="teaser__location">${item.location}</p>
+                <p>${item.start} u.</p>
+            </div>
+        </div>
+    </div>
+    `)
+
+    // HTML for the entire category
     return `
     <div class="event-category" id="${category}">
         <div class="event-category__title">
@@ -53,37 +72,41 @@ function generateHTMLForEvents(items, category) {
             <a class="go-up" href="#filters"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 32"><path d="M17.8 9.9 8.94 0 0 9.9l2.23 2L7.4 6.15V32h3V6.14l5.17 5.76z"/></svg></a>
         </div>
         <div class="event-category__teasers">
-            ${items.filter((item) => item.category === category).map((item) => `
-            <div class="teaser__wrapper">
-                <span class="teaser__date">${item.day_of_week} ${item.day} juli</span>
-                <img class="teaser__img" src="${item.image ? item.image.thumb : ''}" alt="">
-                <div class="teaser">
-                    <h3>${item.title}</h3>
-                    <div>
-                        <p class="teaser__location">${item.location}</p>
-                        <p>${item.start} u.</p>
-                    </div>
-                </div>
-            </div>
-            `).join('')}
+            ${teasersHTML}
         </div>
-        
     </div>
     `
-}
+};
 
 function renderEvents(data) {
     // Filter events on selected day
     const filteredEvents = filteredEventsByDay(data, selectedDay)
     // Extract unique categories from the events array
     categories = [...new Set(filteredEvents.map(event => event.category))];
+    console.log(categories);
     // Used to accumulate all HTML for all categories
     let eventsHTML = '';
     for (const category of categories) {
         eventsHTML += generateHTMLForEvents(filteredEvents, category);
     };
     $events.innerHTML = eventsHTML;
-}
+};
+
+// ---------------- ACTIVE VIEW (calendar) ----------------------------------------------------------------------------------------------------------------
+function activeCalendarLink(selectedDay) {
+    const $calendarLinks = document.querySelectorAll('.calendar-link')
+
+    // First remove all links with calendar-link--active
+    $calendarLinks.forEach(link => {
+        link.classList.remove('calendar-link--active')
+    });
+
+    // Add calendar-link--active to the selected link
+    const selectedLink = document.getElementById(selectedDay)
+    if (selectedLink) {
+        selectedLink.classList.add('calendar-link--active')
+    }
+};
 
 // ---------------- INITIALIZE APPLICATION ----------------------------------------------------------------------------------------------------------------
 // Start the application
@@ -92,7 +115,10 @@ function initialize () {
     changeEventsView()
     // Load the events from the API
     const api = API_URL;
-    fetchData(api, renderEvents);
+    fetchData(api, data => {
+        renderEvents(data);
+        activeCalendarLink(selectedDay);
+    });
     console.log(selectedDay);
 };
 

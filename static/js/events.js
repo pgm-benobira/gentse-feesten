fb0c8486ebcf6783799cb65eca968d40369d835e
pgm-benobira <benoit.biragumaihimbazwe@student.arteveldehs.be>
2024-01-08 00:59:14 +0100

    ✨ Added events list view (toggle)

--- static/js/events.js
+++ static/js/events.js
@@ -56,10 +56,9 @@ function generateHTMLForEvents(items, category) {
         <img class="teaser__img" src="${item.image ? item.image.thumb : ''}" alt="">
         <div class="teaser">
             <h3>${item.title}</h3>
-            <div>
-                <p class="teaser__location">${item.location}</p>
-                <p>${item.start} u.</p>
-            </div>
+            <p class="teaser__location">${item.location}</p>
+            <p class="teaser__start">${item.start} u.</p>
+            ${item.wheelchair_accessible ? `<svg class="teaser__paid" fill="white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 32"><path d="M20.68 27.23c-4.46 0-8-2.35-9.72-6.01h11.76v-3.8H9.9c-.09-.45-.09-.93-.09-1.42 0-.44 0-.88.05-1.33h12.86v-3.8H10.87a10.53 10.53 0 0 1 9.81-6.1c4.38 0 7.83 2.35 9.5 5.97h5.36C33.59 4.34 27.89 0 20.73 0 13.39 0 7.56 4.42 5.53 10.87H0v3.8h4.82c-.05.45-.05.89-.05 1.33 0 .49 0 .97.05 1.42H0v3.8h5.57C7.6 27.62 13.39 32 20.73 32c7.16 0 12.86-4.33 14.8-10.74H30.2a10.16 10.16 0 0 1-9.5 5.97z"/></svg>` : ''}
         </div>
     </a>
     `)

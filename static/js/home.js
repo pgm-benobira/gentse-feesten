7f871705f71c06155688f9520f1a8b8bf23e7f69
pgm-benobira <benoit.biragumaihimbazwe@student.arteveldehs.be>
2024-01-08 21:34:46 +0100

    ✨ Added renderLastThreeNewsItems function for index.html

--- static/js/home.js
+++ static/js/home.js
@@ -2,20 +2,20 @@
 const $randomEvents = document.getElementById('random-events');
 
 // ---------------- MOVE SCROLL ---------------------------------------------------------------------------------------------------------------------------
-const $moveRightElement = document.getElementById('arrow--right');
-const $moveLeftElement = document.getElementById('arrow--left');
-
 function scrollEvents() {
+    const $moveRightElement = document.getElementById('arrow--right');
+    const $moveLeftElement = document.getElementById('arrow--left');
     $moveRightElement.addEventListener('click', () => {
         $randomEvents.scrollLeft += 420;
     });
     $moveLeftElement.addEventListener('click', () => {
         $randomEvents.scrollLeft += -420;
     });
-}
+};
 
 // ---------------- API URL -------------------------------------------------------------------------------------------------------------------------------
 const API_URL_EVENTS = 'https://www.pgm.gent/data/gentsefeesten/events.json';
+const API_URL_NEWS = 'https://www.pgm.gent/data/gentsefeesten/news.json';
 
 // ---------------- FETCH THE DATA ------------------------------------------------------------------------------------------------------------------------
 async function fetchData(url, callback) {
@@ -35,9 +35,9 @@ async function fetchData(url, callback) {
 // ---------------- RANDOM DAY EVENTS ---------------------------------------------------------------------------------------------------------------------
 function renderRandomEvents(amount, data) {
     // Shuffle the events (https://www.freecodecamp.org/news/how-to-shuffle-an-array-of-items-using-javascript-or-typescript/)
-    const shuffledEvents = data.sort(() => Math.random() - 0.5)
+    const shuffledEvents = data.sort(() => Math.random() - 0.5);
     // Take the first three from the shuffled events array
-    const randomEvents = shuffledEvents.slice(0, amount)
+    const randomEvents = shuffledEvents.slice(0, amount);
     const randomEventsHTML = randomEvents.map((item) => `
         <a href="detail.html?day=${item.day}&slug=${item.slug}" class="teaser__wrapper">
             <span class="teaser__date">${item.day_of_week} ${item.day} juli</span>
@@ -53,6 +53,27 @@ function renderRandomEvents(amount, data) {
     $randomEvents.innerHTML = randomEventsHTML;
 };
 
+// ---------------- RANDOM DAY EVENTS ---------------------------------------------------------------------------------------------------------------------
+function renderLastThreeNewsItems(amount, data) {
+    const $newsGridElement = document.getElementById('news__grid');
+    const threeNewsItems = data.slice(0, amount);
+    const threeNewsItemsHTML = threeNewsItems.map((item, index) => `
+    <a href="./news.html" class="news-item news-item--${index + 1}">
+        <span>${item.title}</span>
+        <div class="arrow">
+            <svg class="arrow--right" viewBox="0 0 94 32"><path d="M86.53 14.15 81.37 9l2.33-2.32 9.36 9.36-9.3 9.3-2.34-2.34 5.44-5.44H0v-3.4z"/></svg>
+        </div>
+    </a>
+    `).join('');
+    const newsGridHTML = `
+    ${threeNewsItemsHTML}
+    <div class="news-photo news-item--4">
+        <img src="./static/img/news-decoration-img.jpg" alt="sfeer-beeld-1">
+    </div>
+    `
+    $newsGridElement.innerHTML = newsGridHTML;
+};
+
 // ---------------- INITIALIZE APPLICATION ----------------------------------------------------------------------------------------------------------------
 // Start the application
 function initialize () {
@@ -61,6 +82,9 @@ function initialize () {
     // Load the events from the API
     fetchData(API_URL_EVENTS, data => {
         renderRandomEvents(8, data);
+    });
+    fetchData(API_URL_NEWS, data => {
+        renderLastThreeNewsItems(3, data);
     });
 };
 

458247ee2d264ad3fb5304b1540696bcada71af3
pgm-benobira <benoit.biragumaihimbazwe@student.arteveldehs.be>
2024-01-08 19:31:25 +0100

    ✨ Added random events function for day.html

--- static/js/events.js
+++ static/js/events.js
@@ -91,6 +91,31 @@ function renderEvents(data) {
     $events.innerHTML = eventsHTML;
 };
 
+// ---------------- RANDOM DAY EVENTS ---------------------------------------------------------------------------------------------------------------------
+const $randomEvents = document.getElementById('random-events')
+
+function renderRandomEvents(data) {
+    // Filter events on selected day
+    const filteredEvents = filteredEventsByDay(data, selectedDay);
+    // Shuffle the events (https://www.freecodecamp.org/news/how-to-shuffle-an-array-of-items-using-javascript-or-typescript/)
+    const shuffledEvents = filteredEvents.sort(() => Math.random() - 0.5)
+    // Take the first three from the shuffled events array
+    const randomEvents = shuffledEvents.slice(0, 3)
+    const randomEventsHTML = randomEvents.map((item) => `
+        <a href="detail.html?day=${item.day}&slug=${item.slug}" class="teaser__wrapper">
+            <span class="teaser__date">${item.day_of_week} ${item.day} juli</span>
+            <img class="teaser__img" src="${item.image ? item.image.thumb : '../static/img/no-event-image.jpg'}" alt="thumb-image-${item.slug}">
+            <div class="teaser">
+                <h3>${item.title}</h3>
+                <p class="teaser__location">${item.location}</p>
+                <p class="teaser__start">${item.start} u.</p>
+                ${item.wheelchair_accessible ? `<svg class="teaser__paid" fill="white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 32"><path d="M20.68 27.23c-4.46 0-8-2.35-9.72-6.01h11.76v-3.8H9.9c-.09-.45-.09-.93-.09-1.42 0-.44 0-.88.05-1.33h12.86v-3.8H10.87a10.53 10.53 0 0 1 9.81-6.1c4.38 0 7.83 2.35 9.5 5.97h5.36C33.59 4.34 27.89 0 20.73 0 13.39 0 7.56 4.42 5.53 10.87H0v3.8h4.82c-.05.45-.05.89-.05 1.33 0 .49 0 .97.05 1.42H0v3.8h5.57C7.6 27.62 13.39 32 20.73 32c7.16 0 12.86-4.33 14.8-10.74H30.2a10.16 10.16 0 0 1-9.5 5.97z"/></svg>` : ''}
+            </div>
+        </a>
+    `).join('');
+    $randomEvents.innerHTML = randomEventsHTML;
+}
+
 // ---------------- ACTIVE VIEW (calendar) ----------------------------------------------------------------------------------------------------------------
 function activeCalendarLink(selectedDay) {
     const $calendarLinks = document.querySelectorAll('.calendar-link')
@@ -124,6 +149,7 @@ function initialize () {
     const api = API_URL;
     fetchData(api, data => {
         renderEvents(data);
+        renderRandomEvents(data)
         activeCalendarLink(selectedDay);
         updatePageTitle(selectedDay);
     });

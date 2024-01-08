e3e55773ff8a67e16e585253b1fdc2bc7e268498
pgm-benobira <benoit.biragumaihimbazwe@student.arteveldehs.be>
2024-01-08 18:42:37 +0100

    📱 Made the raster and list events responsive

--- static/js/detail.js
+++ static/js/detail.js
@@ -2,7 +2,7 @@
 const $eventDetail = document.getElementById('event-detail');
 
 // ---------------- API URL -------------------------------------------------------------------------------------------------------------------------------
-const API_URL = 'https://www.pgm.gent/data/gentsefeesten/events_500.json';
+const API_URL = 'https://www.pgm.gent/data/gentsefeesten/events.json';
 
 // ---------------- FETCH THE DATA ------------------------------------------------------------------------------------------------------------------------
 async function fetchData(url, callback) {
@@ -25,7 +25,7 @@ let selectedDay = urlParams.get('day') ? urlParams.get('day') : '14';
 let selectedEvent = urlParams.get('slug')
 
 function filteredEventsBySlug(data, slug) {
-    return data.find((item) => item.slug === slug);
+    return data.find((item) => item.slug === slug && item.day === selectedDay);
 };
 
 // ---------------- EVENTS --------------------------------------------------------------------------------------------------------------------------------
@@ -60,7 +60,7 @@ function generateHTMLForEventDetail(item) {
             </div>
         </section>
         <section class="event-detail__right">
-            <img class="event-detail__photo" src="${item.image ? item.image.full : ''}" alt="foto-${item.slug}">
+            <img class="event-detail__photo" loading="lazy" src="${item.image ? item.image.full : ''}" alt="foto-${item.slug}">
             <ul class="socials socials--event">
                 <li><a href="#" target="_blank"><img src="../static/img/gentse-feesten-icoontjes/twitter.svg" alt="twitter-logo"></a></li>
                 <li><a href="#" target="_blank"><img src="../static/img/gentse-feesten-icoontjes/facebook.svg" alt="facebook-logo"></a></li>
@@ -82,7 +82,7 @@ function generateHTMLForEventDetail(item) {
                     ${item.wheelchair_accessible ? `<div class="event-accessibility__container"><svg class="event-accessibility" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path d="m31 24.1.9 1.8a1 1 0 0 1-.45 1.34l-4.1 2.05a2 2 0 0 1-2.7-.94L20.73 20H12a2 2 0 0 1-1.98-1.72C7.9 3.45 8.02 4.38 8 4a4 4 0 1 1 4.59 3.96l.29 2.04H21a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1h-7.55l.3 2H22c.78 0 1.48.45 1.82 1.15l3.6 7.65 2.25-1.14a1 1 0 0 1 1.34.45zM19.47 22h-1.53A7.01 7.01 0 0 1 4 21c0-2.6 1.42-4.86 3.52-6.08l-.6-4.14A11.03 11.03 0 0 0 0 21a11.01 11.01 0 0 0 21.07 4.43L19.47 22z"/></svg></div>`  : ''}
                 </div>
                 <p class="event-description">${item.description}</p>
-                <img class="event-detail__photo" src="${item.image ? item.image.full : ''}" alt="foto-${item.slug}">
+                <img class="event-detail__photo" loading="lazy" src="${item.image ? item.image.full : ''}" alt="foto-${item.slug}">
             </div>
         </section>
         <section class="event-detail__right">

f046bc0ed76a572f25b01bed44b83b2d3b4503b5
pgm-benobira <benoit.biragumaihimbazwe@student.arteveldehs.be>
2024-01-10 16:13:44 +0100

    ♻️ Refactoring detail.js

--- static/js/detail.js
+++ static/js/detail.js
@@ -48,81 +48,58 @@ function handleURLParams(data) {
 };
 
 // ---------------- EVENTS --------------------------------------------------------------------------------------------------------------------------------
-function generateHTMLForEventDetail(item) {
-    categoriesHTML = item.category.map((c) => `<a href="./day.html?day=${item.day}#${c}">${c}</a>`).join('\n')
+function generateHTMLForEventDetailGoBack(item) {
     return `
-    <div class="segment__inner segment__inner--newspage event-detail">
-        <section class="event-detail__left">
-            <a href="./day.html?day=${item.day}" class="go-back">
-                <svg class="go-back__arrow" viewBox="0 0 94 32"><path d="M86.53 14.15 81.37 9l2.33-2.32 9.36 9.36-9.3 9.3-2.34-2.34 5.44-5.44H0v-3.4z"/></svg>
-                <p>Overzicht ${item.day_of_week} ${item.day} juli</p>
-            </a>
-            <div class="event-detail__content">
-                <h2>${item.title}</h2>
-                <div class="event-space-time">
-                    <a href="#" class="event-space">${item.location}</a>
-                    <p class="event-time">${item.start} u. - ${item.end} u.</p>
-                    ${item.wheelchair_accessible ? `<div class="event-accessibility__container"><svg class="event-accessibility" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path d="m31 24.1.9 1.8a1 1 0 0 1-.45 1.34l-4.1 2.05a2 2 0 0 1-2.7-.94L20.73 20H12a2 2 0 0 1-1.98-1.72C7.9 3.45 8.02 4.38 8 4a4 4 0 1 1 4.59 3.96l.29 2.04H21a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1h-7.55l.3 2H22c.78 0 1.48.45 1.82 1.15l3.6 7.65 2.25-1.14a1 1 0 0 1 1.34.45zM19.47 22h-1.53A7.01 7.01 0 0 1 4 21c0-2.6 1.42-4.86 3.52-6.08l-.6-4.14A11.03 11.03 0 0 0 0 21a11.01 11.01 0 0 0 21.07 4.43L19.47 22z"/></svg></div>`  : ''}
-                </div>
-                <p class="event-description">${item.description}</p>
-                <div class="event-info">
-                    <p class="event-info__key">Organisator:</p>
-                    <p>${item.organizer}</p>
-                </div>
-                ${item.url ? `<div class="event-info"><p class="event-info__key">Website:</p><a href="${item.url}" target="_blank">${item.url}</a></div>`: ''}
-                <div class="event-info">
-                    <p class="event-info__key">Categorieën:</p>
-                    <div class="event-info__categories">
-                        ${categoriesHTML}
-                    </div>
-                </div>
-            </div>
-        </section>
-        <section class="event-detail__right">
-            <img class="event-detail__photo" loading="lazy" src="${item.image ? item.image.full : ''}" alt="foto-${item.slug}">
-            <ul class="socials socials--event">
-                <li><a href="#" target="_blank"><img src="../static/img/gentse-feesten-icoontjes/twitter.svg" alt="twitter-logo"></a></li>
-                <li><a href="#" target="_blank"><img src="../static/img/gentse-feesten-icoontjes/facebook.svg" alt="facebook-logo"></a></li>
-                <li><a href="#" target="_blank"><img src="../static/img/gentse-feesten-icoontjes/pinterest.svg" alt="pinterest-logo"></a></li>
-            </ul>
-        </section>
+    <a href="./day.html?day=${item.day}" class="go-back">
+        <svg class="go-back__arrow" viewBox="0 0 94 32"><path d="M86.53 14.15 81.37 9l2.33-2.32 9.36 9.36-9.3 9.3-2.34-2.34 5.44-5.44H0v-3.4z"/></svg>
+        <p>Overzicht ${item.day_of_week} ${item.day} juli</p>
+    </a>
+    `
+}
+
+function generateHTMLForEventDetailSpaceTime(item) {
+    return `
+    <div class="event-space-time">
+        <a href="#" class="event-space">${item.location}</a>
+        <p class="event-time">${item.start} u. - ${item.end} u.</p>
+        ${item.wheelchair_accessible ? `<div class="event-accessibility__container"><svg class="event-accessibility" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path d="m31 24.1.9 1.8a1 1 0 0 1-.45 1.34l-4.1 2.05a2 2 0 0 1-2.7-.94L20.73 20H12a2 2 0 0 1-1.98-1.72C7.9 3.45 8.02 4.38 8 4a4 4 0 1 1 4.59 3.96l.29 2.04H21a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1h-7.55l.3 2H22c.78 0 1.48.45 1.82 1.15l3.6 7.65 2.25-1.14a1 1 0 0 1 1.34.45zM19.47 22h-1.53A7.01 7.01 0 0 1 4 21c0-2.6 1.42-4.86 3.52-6.08l-.6-4.14A11.03 11.03 0 0 0 0 21a11.01 11.01 0 0 0 21.07 4.43L19.47 22z"/></svg></div>`  : ''}
     </div>
-    <div class="segment__inner segment__inner--newspage event-detail event-detail--mobile">
-        <section class="event-detail__left">
-            <a href="./day.html?day=${item.day}" class="go-back">
-                <svg class="go-back__arrow" viewBox="0 0 94 32"><path d="M86.53 14.15 81.37 9l2.33-2.32 9.36 9.36-9.3 9.3-2.34-2.34 5.44-5.44H0v-3.4z"/></svg>
-                <p>Overzicht ${item.day_of_week} ${item.day} juli</p>
-            </a>
-            <div class="event-detail__content">
-                <h2>${item.title}</h2>
-                <div class="event-space-time">
-                    <a href="#" class="event-space">${item.location}</a>
-                    <p class="event-time">${item.start} u. - ${item.end} u.</p>
-                    ${item.wheelchair_accessible ? `<div class="event-accessibility__container"><svg class="event-accessibility" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path d="m31 24.1.9 1.8a1 1 0 0 1-.45 1.34l-4.1 2.05a2 2 0 0 1-2.7-.94L20.73 20H12a2 2 0 0 1-1.98-1.72C7.9 3.45 8.02 4.38 8 4a4 4 0 1 1 4.59 3.96l.29 2.04H21a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1h-7.55l.3 2H22c.78 0 1.48.45 1.82 1.15l3.6 7.65 2.25-1.14a1 1 0 0 1 1.34.45zM19.47 22h-1.53A7.01 7.01 0 0 1 4 21c0-2.6 1.42-4.86 3.52-6.08l-.6-4.14A11.03 11.03 0 0 0 0 21a11.01 11.01 0 0 0 21.07 4.43L19.47 22z"/></svg></div>`  : ''}
-                </div>
-                <p class="event-description">${item.description}</p>
-                <img class="event-detail__photo" loading="lazy" src="${item.image ? item.image.full : ''}" alt="foto-${item.slug}">
-            </div>
-        </section>
-        <section class="event-detail__right">
-            <div class="event-info">
-                <p class="event-info__key">Organisator:</p>
-                <p>${item.organizer}</p>
-            </div>
-            ${item.url ? `<div class="event-info"><p class="event-info__key">Website:</p><a href="${item.url}" target="_blank">${item.url}</a></div>`: ''}
-            <div class="event-info">
-                <p class="event-info__key">Categorieën:</p>
-                <div class="event-info__categories">
-                    ${categoriesHTML}
-                </div>
-            </div>
-            <ul class="socials socials--event">
-                <li><a href="#" target="_blank"><img src="../static/img/gentse-feesten-icoontjes/twitter.svg" alt="twitter-logo"></a></li>
-                <li><a href="#" target="_blank"><img src="../static/img/gentse-feesten-icoontjes/facebook.svg" alt="facebook-logo"></a></li>
-                <li><a href="#" target="_blank"><img src="../static/img/gentse-feesten-icoontjes/pinterest.svg" alt="pinterest-logo"></a></li>
-            </ul>
-        </section>
+    `
+}
+
+function generateHTMLForEventDetailOrganizer(item) {
+    return `
+    <div class="event-info">
+        <p class="event-info__key">Organisator:</p>
+        <p>${item.organizer}</p>
     </div>
+    `
+}
+
+function generateHTMLForEventDetailCategories(item) {
+    const categoriesHTML = item.category.map((c) => `<a href="./day.html?day=${item.day}#${c}">${c}</a>`).join('\n');
+    return `
+    <div class="event-info">
+        <p class="event-info__key">Categorieën:</p>
+        <div class="event-info__categories">
+            ${categoriesHTML}
+        </div>
+    </div>
+    `
+};
+
+function generateHTMLForEventDetailSocials() {
+    return `
+    <ul class="socials socials--event">
+        <li><a href="#" target="_blank"><img src="../static/img/gentse-feesten-icoontjes/twitter.svg" alt="twitter-logo"></a></li>
+        <li><a href="#" target="_blank"><img src="../static/img/gentse-feesten-icoontjes/facebook.svg" alt="facebook-logo"></a></li>
+        <li><a href="#" target="_blank"><img src="../static/img/gentse-feesten-icoontjes/pinterest.svg" alt="pinterest-logo"></a></li>
+    </ul>
+    `
+};
+
+function generateHTMLForEventDetailMap(item) {
+    return `
     <div class="segment__inner segment__inner--newspage map">
         <section class="location">
             <a href="#" class="event-space event-space--map">${item.location}</a>
@@ -132,6 +109,52 @@ function generateHTMLForEventDetail(item) {
             <img src="../static/img/map-decoration-img.png" alt="map-of-ghent">
         </section>
     </div>
+    `
+};
+
+function generateHTMLForEventDetailMobile(item) {
+    return `
+    <div class="segment__inner segment__inner--newspage event-detail event-detail--mobile">
+        <section class="event-detail__left">
+            ${generateHTMLForEventDetailGoBack(item)}
+            <div class="event-detail__content">
+                <h2>${item.title}</h2>
+                ${generateHTMLForEventDetailSpaceTime(item)}
+                <p class="event-description">${item.description}</p>
+                <img class="event-detail__photo" loading="lazy" src="${item.image ? item.image.full : ''}" alt="foto-${item.slug}">
+            </div>
+        </section>
+        <section class="event-detail__right">
+            ${generateHTMLForEventDetailOrganizer(item)}
+            ${item.url ? `<div class="event-info"><p class="event-info__key">Website:</p><a href="${item.url}" target="_blank">${item.url}</a></div>`: ''}
+            ${generateHTMLForEventDetailCategories(item)}
+            ${generateHTMLForEventDetailSocials()}
+        </section>
+    </div>
+    `
+};
+
+function generateHTMLForEventDetail(item) {
+    return `
+    <div class="segment__inner segment__inner--newspage event-detail">
+        <section class="event-detail__left">
+            ${generateHTMLForEventDetailGoBack(item)}
+            <div class="event-detail__content">
+                <h2>${item.title}</h2>
+                ${generateHTMLForEventDetailSpaceTime(item)}
+                <p class="event-description">${item.description}</p>
+                ${generateHTMLForEventDetailOrganizer(item)}
+                ${item.url ? `<div class="event-info"><p class="event-info__key">Website:</p><a href="${item.url}" target="_blank">${item.url}</a></div>`: ''}
+                ${generateHTMLForEventDetailCategories(item)}
+            </div>
+        </section>
+        <section class="event-detail__right">
+            <img class="event-detail__photo" loading="lazy" src="${item.image ? item.image.full : ''}" alt="foto-${item.slug}">
+            ${generateHTMLForEventDetailSocials()}
+        </section>
+    </div>
+    ${generateHTMLForEventDetailMobile(item)}
+    ${generateHTMLForEventDetailMap(item)}
     `
 };
 
@@ -163,7 +186,7 @@ function updatePageTitle(selectedEvent) {
 // ---------------- INITIALIZE APPLICATION ----------------------------------------------------------------------------------------------------------------
 // Start the application
 function initialize () {
-    // Load the events from the API
+    // Load from the events API
     const api = API_URL;
     fetchData(api, data => {
         const selectedEventData = selectedEvent ? filteredEventsBySlug(data, selectedEvent) : ``;
@@ -173,8 +196,6 @@ function initialize () {
         activeCalendarLink(selectedDay);
         updatePageTitle(selectedEventData.title);
     });
-    // Handle wrong URLParams
-    
     // Console checks
     console.log('Day:', selectedDay);
     console.log('Event:', selectedEvent)
